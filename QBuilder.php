f44bd4d8804f4669357c6a9806c1f86b981e87d2
develhoper <develhoper.0@gmail.com>
2019-11-11 18:15:02 +0330

    where clause and prepared statement

--- QBuilder.php
+++ QBuilder.php
@@ -4,7 +4,7 @@ class QBuilder{
     protected $db;
     protected $table;
     protected $query;
-    
+    protected $params=[];
     public function __construct()
     {
         $this->db=DB::getInstance();
@@ -19,17 +19,37 @@ class QBuilder{
         return $this;
     }
 
+    public function where(...$cond){
+        $condition=" where ";
+        if(count($cond)==2){
+            $this->params[":cond"]=$cond[1];
+            $condition.="$cond[0]=:cond";
+        }else{
+            $this->params[":cond"] = $cond[2];
+            $condition.="$cond[0]$cond[1]:cond";
+        }
+        $this->query.=$condition." ";
+        return $this;
+    }
+
+    public function first(){
+        $this->query.=" limit 1";
+        return $this->execute()->fetchObject(get_class($this));
+    }
+
+    public function get(){
+        return $this->execute()->fetchAll(PDO::FETCH_CLASS,get_class($this));
+    }
+
     public function execute(){
         $stmt=$this->db->prepare($this->query);
-        $stmt->execute();
+        $stmt->execute($this->params);
         return $stmt;
     }
 
-    public static function all(){
-        $builder=new QBuilder();
-        $builder->table="users";
-        $stmt=$builder->select()->execute();
-        return $stmt->fetchAll(PDO::FETCH_CLASS,get_called_class());
+    public function all(){
+        $stmt=$this->select()->execute();
+        return $stmt->fetchAll(PDO::FETCH_CLASS,get_class($this));
     }
 
 }
--- test/test.php
+++ test/test.php
@@ -3,5 +3,7 @@
 use app\models\User;
 
 require '../load.php';
-$users=User::all();
-var_dump($users);
+$user=new User();
+// $r=$user->select()->where("id",">=",2)->get();
+$r=$user->all();
+var_dump($r);
