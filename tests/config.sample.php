f370a7f30605163c70c0c8da1f9378d99accd90b
develhopper <develhoper.0@gmail.com>
2020-10-01 20:48:13 +0330

    config.sample.php instead of config.php

---
+++ tests/config.sample.php
@@ -0,0 +1,11 @@
+<?php
+define("DB_DRIVER","database_driver");
+define("DB_HOST","database_host");
+define("DB_USER","database_user");
+define("DB_PASSWORD","database_password");
+define("DB_NAME","database_name");
+
+function debug($msg){
+    echo "**DEBUG $msg **";
+}
+?>
